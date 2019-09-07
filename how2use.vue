cf7a03d6d1cc108eb46155916179f5c19072f379
ZUXING <53240486+ZUXING@users.noreply.github.com>
2019-09-07 20:24:39 +0800

    VUE-自动添加阴影的控件
    将原有的HTML+CSS+JS的控件改写为VUE插件的形式

---
+++ how2use.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <shadowBox content="台灯" width="200" height="40" shifting="5" selected="selected"/>
+    <!--
+    	content="阴影盒子的内容"
+    	width="宽度（单位：像素）"
+    	height="高度（单位：像素）"
+    	shifting="偏移量（单位：像素）"
+    	selected="被选中的状态（尚未实现）"
+
+    	使用时请手动给shadowBox插件留出它应有的高度值
+    	否则在浏览器中shadowBox插件本身的高度值是0
+    -->
+  </div>
+</template>
+
+<script>
+import shadowBox from './components/shadowBox.vue'
+
+export default {
+  name: 'app',
+  components: {
+    shadowBox
+  }
+}
+</script>
---
+++ shadowBox.vue
@@ -0,0 +1,45 @@
+<template>
+	<div :style='{width:fullWidth+"px",height:fullHeight+"px"}'>
+		<div class="content" :style='{width:width+"px",height:height+"px",lineHeight:height+"px"}'>{{content}}</div>
+		<div class='shadow' :style='{width:width+"px",height:height+"px",top:shifting+"px",left:shifting+"px"}'></div>
+	</div>
+</template>
+
+<script>
+export default {
+	props:["content","width","height","shifting","selected"],
+	/*data(){
+		let fullWidth=parseInt(width)+parseInt(shifting)
+		let fullHeight=parseInt(height)+parseInt(shifting)
+		return{
+			fullWidth,
+			fullHeight
+		}
+	}*/
+}
+</script>
+
+<style scoped>
+	div{
+		text-align: center;
+		position: relative;
+	}
+	div.content{
+		background-color: #FFF;
+		border: 2px solid #333;
+		position: absolute;
+		z-index: 2;
+	}
+	div.shadow{
+		background-image: url(./bgp.png);
+		border: 2px solid #333;
+		position: absolute;
+		z-index: 1;
+	}
+	
+	div.selected{
+		color: #FFF;
+		background-color: #ff6100;
+	}
+	div.box-content *{vertical-align: top;}
+</style>
